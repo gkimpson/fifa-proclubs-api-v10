b510d7edda9f55415b96555f84c471c288da0f43
Gavin Kimpson <gkimpson@gmail.com>
2023-03-09 20:07:50 +0000

    wip - dashboard results view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,8 +10,22 @@ class DashboardController extends Controller
     public function index(): View
     {
         $data = [
-            'results' => Result::getAll()->paginate(20),
+            'results' => Result::getAll()->paginate(1),
         ];
+
+//        dump($data['results'][0]->properties['clubs'][0]['name']);
+        /**
+         * @todo
+         * 0 - Club name, Total Pts, Total Wins, Draws, Losses
+         * 1 - Div ranking (Current Div, Season No, Pts, Games remaining & W-L-D form, pts to next division/title)
+         * 2 - Members (random member on dashboard, total members, members by position, link to view all members)
+         * 3 - Last Match (scoreline, link to all matches)
+         * 4 - Trophies (if any)
+         * 5 - Club history (Seasons played, total games, titles won, highest pts total, promotions, relegations)
+         * 6 - Club Logo with W-L-D % in a circular graph around the emblem
+         * 7 - Best season finish with animation like EA site
+         * 8 - look into caching....
+         */
 
         return view('dashboard.index', $data);
     }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -17,6 +17,25 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex flex-row">
+                <div class="basis-1/4 text-center">HOME</div>
+                <div class="basis-1/2 text-center"></div>
+                <div class="basis-1/4 text-center">AWAY</div>
+            </div>
+            @foreach ($results as $result)
+                <div class="flex flex-row">
+                    <div class="basis-1/3 text-center">{{ $result->properties['clubs'][0]['name'] }}</div>
+                    <div class="basis-1/3 text-center">{{ $result->home_team_goals }} - {{ $result->away_team_goals }}</div>
+                    <div class="basis-1/3 text-center">{{ $result->properties['clubs'][1]['name'] }}</div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+
+    <!-- Start Results -->
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div>
@@ -28,5 +47,6 @@
             </div>
         </div>
     </div>
+    <!-- End Results -->
 
 </x-app-layout>
