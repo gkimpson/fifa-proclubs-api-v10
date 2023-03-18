5573af185c91dc7dea60fdb6c4ab386739f82d40
Gavin Kimpson <gkimpson@gmail.com>
2023-03-18 08:30:02 +0000

    remove the additional forward slash in the routes

--- routes/web.php
+++ routes/web.php
@@ -22,31 +22,31 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/results', [ResultController::class, 'index'])->name('results.index');
+Route::get('results', [ResultController::class, 'index'])->name('results.index');
 
 Route::prefix('club/{clubId}/platform/{platform}')->group(function () {
     Route::get('/', [ClubController::class, 'index'])->name('club.index');
-    Route::get('/career', [ClubController::class, 'career'])->name('club.career');
-    Route::get('/cup', [ClubController::class, 'cup'])->name('club.cup');
-    Route::get('/form', [ClubController::class, 'form'])->name('club.form');
-    Route::get('/league', [ClubController::class, 'league'])->name('club.league');
-    Route::get('/members', [ClubController::class, 'members'])->name('club.members');
-    Route::get('/players/{player}', [ClubController::class, 'player'])->name('club.players');
-    Route::get('/rankings', [ClubController::class, 'rankings'])->name('club.rankings');
-    Route::get('/search', [ClubController::class, 'search'])->name('club.search');
-    Route::get('/season', [ClubController::class, 'season'])->name('club.season');
-    Route::get('/settings', [ClubController::class, 'settings'])->name('club.settings');
-    Route::get('/squad', [ClubController::class, 'squad'])->name('club.squad');
-    Route::get('/squad/compare/{player1}/{player2}', [ClubController::class, 'compare'])->name('club.squad.compare');
-    Route::get('/squad/ranking', [ClubController::class, 'ranking'])->name('club.squad.ranking');
+    Route::get('career', [ClubController::class, 'career'])->name('club.career');
+    Route::get('cup', [ClubController::class, 'cup'])->name('club.cup');
+    Route::get('form', [ClubController::class, 'form'])->name('club.form');
+    Route::get('league', [ClubController::class, 'league'])->name('club.league');
+    Route::get('members', [ClubController::class, 'members'])->name('club.members');
+    Route::get('players/{player}', [ClubController::class, 'player'])->name('club.players');
+    Route::get('rankings', [ClubController::class, 'rankings'])->name('club.rankings');
+    Route::get('search', [ClubController::class, 'search'])->name('club.search');
+    Route::get('season', [ClubController::class, 'season'])->name('club.season');
+    Route::get('settings', [ClubController::class, 'settings'])->name('club.settings');
+    Route::get('squad', [ClubController::class, 'squad'])->name('club.squad');
+    Route::get('squad/compare/{player1}/{player2}', [ClubController::class, 'compare'])->name('club.squad.compare');
+    Route::get('squad/ranking', [ClubController::class, 'ranking'])->name('club.squad.ranking');
 });
 
-Route::get('/platform/{platform}/leaderboard/{leaderboardType}/', [ClubController::class, 'leaderboard'])->name('club.leaderboard');
+Route::get('platform/{platform}/leaderboard/{leaderboardType}/', [ClubController::class, 'leaderboard'])->name('club.leaderboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
