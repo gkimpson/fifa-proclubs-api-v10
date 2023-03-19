90f44ae84961e259c3779ebce3e6ac705208b7e6
Gavin Kimpson <gkimpson@gmail.com>
2023-03-19 15:32:24 +0000

    duster cleanup

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ResultResource;
 use App\Models\Result;
-use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index(): ResultResource
     {
         $results = Result::get()->paginate(10);
+
         return new ResultResource($results);
     }
 }
