d0835240c1467f7652d0c0d888af898b6d3c627a
Gavin Kimpson <gkimpson@gmail.com>
2023-03-19 16:14:10 +0000

    added custom seeder to single row into users table

---
+++ public/sql/customdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `name`, `email`, `platform`, `club_id`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`)
+VALUES
+    (1, 'Joe Bloggs', 'user@app.com', 'ps5', 52003, NULL, '$2y$10$6GH42SG2.TYAyxAmbErIr.WMDVqfPbrJa02NaGXiSD8DAuKoeWCBa', NULL, '2023-03-05 17:25:03', '2023-03-05 17:25:03');
