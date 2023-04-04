e0acfbec99cd9ee185429626c9499763f5917668
Gavin Kimpson <gkimpson@gmail.com>
2023-04-05 00:08:45 +0100

    fix dupe route

--- routes/api.php
+++ routes/api.php
@@ -27,19 +27,19 @@ Route::get('results', [ResultController::class, 'index']);
 Route::get('results/{result}', [ResultController::class, 'show']);
 
 Route::prefix('club/{clubId}/platform/{platform}')->group(function () {
-    Route::get('/', [ClubController::class, 'index'])->name('club.index');
-    Route::get('career', [ClubController::class, 'career'])->name('club.career');
-    Route::get('cup', [ClubController::class, 'cup'])->name('club.cup');
-    Route::get('form', [ClubController::class, 'form'])->name('club.form');
-    Route::get('league', [ClubController::class, 'league'])->name('club.league');
-    Route::get('members', [ClubController::class, 'members'])->name('club.members');
-    Route::get('players/{player}', [ClubController::class, 'player'])->name('club.players');
-    Route::get('search', [ClubController::class, 'search'])->name('club.search');
-    Route::get('season', [ClubController::class, 'season'])->name('club.season');
-    Route::get('settings', [ClubController::class, 'settings'])->name('club.settings');
-    Route::get('squad', [ClubController::class, 'squad'])->name('club.squad');
-    Route::get('squad/compare/{player1}/{player2}', [ClubController::class, 'compare'])->name('club.squad.compare');
-    Route::get('squad/ranking', [ClubController::class, 'ranking'])->name('club.squad.ranking');
+    Route::get('/', [ClubController::class, 'index'])->name('api.club.index');
+    Route::get('career', [ClubController::class, 'career'])->name('api.club.career');
+    Route::get('cup', [ClubController::class, 'cup'])->name('api.club.cup');
+    Route::get('form', [ClubController::class, 'form'])->name('api.club.form');
+    Route::get('league', [ClubController::class, 'league'])->name('api.club.league');
+    Route::get('members', [ClubController::class, 'members'])->name('api.club.members');
+    Route::get('players/{player}', [ClubController::class, 'player'])->name('api.club.players');
+    Route::get('search', [ClubController::class, 'search'])->name('api.club.search');
+    Route::get('season', [ClubController::class, 'season'])->name('api.club.season');
+    Route::get('settings', [ClubController::class, 'settings'])->name('api.club.settings');
+    Route::get('squad', [ClubController::class, 'squad'])->name('api.club.squad');
+    Route::get('squad/compare/{player1}/{player2}', [ClubController::class, 'compare'])->name('api.club.squad.compare');
+    Route::get('squad/ranking', [ClubController::class, 'ranking'])->name('api.club.squad.ranking');
 });
 
-Route::get('platform/{platform}/leaderboard/{leaderboardType}/', [ClubController::class, 'leaderboard'])->name('club.leaderboard');
+Route::get('platform/{platform}/leaderboard/{leaderboardType}/', [ClubController::class, 'leaderboard'])->name('api.club.leaderboard');
