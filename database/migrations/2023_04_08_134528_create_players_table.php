0527f28a9e38702519740e3f01d4add9553c0822
Gavin Kimpson <gkimpson@gmail.com>
2023-04-08 16:14:58 +0100

    migration for players table

---
+++ database/migrations/2023_04_08_134528_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Enums\Platforms;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('club_id');
+            $table->enum('platform', Platforms::all());
+            $table->string('player_name', 255);
+            $table->string('attributes', 255);
+            $table->timestamps();
+
+            $table->unique(['club_id', 'platform', 'player_name'], 'idx_unique_player_by_club_and_platform');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
