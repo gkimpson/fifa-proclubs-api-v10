66933ef03a36db701f0151c0205896875f3f25ad
Gavin Kimpson <gkimpson@gmail.com>
2023-04-12 23:05:06 +0100

    unsure why the Http facade plays up online... late so will debug another time

--- app/Services/ProclubsApiService.php
+++ app/Services/ProclubsApiService.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Log;
 class ProclubsApiService
 {
     public const API_URL = 'https://proclubs.ea.com/api/fifa/';
+    public const REFERER = 'https://www.ea.com/';
 
     // TODO - add to an ENUM later
     public function __construct()
@@ -19,6 +20,67 @@ class ProclubsApiService
     }
 
     public static function doExternalApiCall(?string $endpoint = null, array $params = [], bool $jsonDecoded = false, bool $isCLI = false)
+    {
+        try {
+            $url = self::API_URL . $endpoint . '?' . http_build_query($params);
+            $curl = curl_init();
+
+            // KEEP THIS BLOCK JUST IN CASE IT NEEDS TO BE USED AGAIN
+            curl_setopt_array($curl, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                // CURLOPT_MAXREDIRS => 5,
+                CURLOPT_TIMEOUT => 10,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+                // CURLOPT_CUSTOMREQUEST => 'GET',
+                // CURLOPT_VERBOSE => false,
+                CURLOPT_FAILONERROR => true,
+                CURLOPT_REFERER => self::REFERER,
+                CURLOPT_HTTPHEADER => [
+                    'accept-language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+                    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+                ],
+            ]);
+
+//            // TODO - use the Laravel way for this just need to figure it out?!, keeping the block above because we all know EA like to mess about like they did before and locked out the ProClub Avengers!
+//            curl_setopt_array($curl, [
+//                CURLOPT_URL => $url,
+//                CURLOPT_RETURNTRANSFER => true,
+//                CURLOPT_TIMEOUT => 5,
+//                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+//                CURLOPT_HTTPHEADER => [
+//                    'accept-language: en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7',
+//                    'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+//                    'Content-Type: application/json',
+//                    'accept: application/json',
+//                ],
+//            ]);
+
+            if (curl_exec($curl) === false || curl_errno($curl)) {
+                echo App::environment(['local', 'staging']) ? 'Curl error: ' . curl_error($curl) : 'An unexpected error has occurred - try again later';
+                Log::error('Curl request failed with last error: ' . curl_error($curl));
+            } else {
+                if ($isCLI) {
+                    echo "Operation completed without any errors\n";
+                }
+            }
+
+            $response = curl_exec($curl);
+            curl_close($curl);
+
+            return $jsonDecoded ? json_decode($response) : $response;
+        } catch (Exception $e) {
+            // do some logging...
+            Log::error('API request failed with exception: ' . $e->getMessage());
+
+            return 0;
+        }
+    }
+
+    // TODO - use the Laravel method - seems to not work on the AWS server but works locally
+    public static function doLaravelExternalApiCall(?string $endpoint = null, array $params = [], bool $jsonDecoded = false, bool $isCLI = false)
     {
         try {
             $url = self::API_URL . $endpoint;
