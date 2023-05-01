c260780af4f4b2bded9ef4d6c441cc6bee73dfd3
Gavin Kimpson <gkimpson@gmail.com>
2023-05-01 15:26:06 +0100

    more unit tests tweaks

--- tests/Unit/Models/ResultTest.php
+++ tests/Unit/Models/ResultTest.php
@@ -14,7 +14,7 @@ class ResultTest extends TestCase
     /**
      * @test
      */
-    public function aSingleResultCanBeAdded(): void
+    public function singleResultCanBeAdded(): void
     {
         Result::factory()->create();
         $this->assertDatabaseCount('results', 1);
@@ -67,5 +67,50 @@ class ResultTest extends TestCase
         Result::factory()->create([
             'match_id' => [],
         ]);
+    }
+
+    /** @test */
+    public function itCanScopeByTeam()
+    {
+        $teamId = 1;
+        Result::factory()->count(3)->create(['home_team_id' => $teamId]);
+        Result::factory()->count(2)->create(['away_team_id' => $teamId]);
+
+        $results = Result::byTeam($teamId)->get();
+
+        $this->assertCount(5, $results);
+        $results->each(function ($result) use ($teamId) {
+            $this->assertTrue($result->home_team_id === $teamId || $result->away_team_id === $teamId);
+        });
+    }
+
+    /** @test */
+    public function itCanScopeHomeTeam()
+    {
+        $teamId = 1;
+        Result::factory()->count(3)->create(['home_team_id' => $teamId]);
+        Result::factory()->count(2)->create(['home_team_id' => 2]);
+
+        $results = Result::homeTeam($teamId)->get();
+
+        $this->assertCount(3, $results);
+        $results->each(function ($result) use ($teamId) {
+            $this->assertEquals($teamId, $result->home_team_id);
+        });
+    }
+
+    /** @test */
+    public function itCanScopeAwayTeam()
+    {
+        $teamId = 1;
+        Result::factory()->count(3)->create(['away_team_id' => $teamId]);
+        Result::factory()->count(2)->create(['away_team_id' => 2]);
+
+        $results = Result::awayTeam($teamId)->get();
+
+        $this->assertCount(3, $results);
+        $results->each(function ($result) use ($teamId) {
+            $this->assertEquals($teamId, $result->away_team_id);
+        });
     }
 }
